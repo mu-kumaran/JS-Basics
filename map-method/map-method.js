a167f5c53e6b4b405fcd0173a15ea1a56c4109db
mu-kumaran <mu.kumaran1706@gmail.com>
2025-06-25 17:30:53 +0530

    Feat: JS basics updation

---
+++ map-method/map-method.js
@@ -0,0 +1,22 @@
+// map() method returns the new array
+
+// Example -1 : Doubles the numbers
+const numbers = [1,2,3,4]
+const doubled = numbers.map(value => value*2)
+console.log(doubled)
+
+// Example - 2 : Convert Strings to UpperCase
+const fruits = ['apple', 'orange', 'banana', 'cherry']
+const FRUITS = fruits.map(value => value.toUpperCase())
+console.log(FRUITS)
+
+// Example - 3 : Extract Property from array of objects
+const users = [
+    {name: "Manoj", age: 25},
+    {name: "Balu", age: 45},
+    {name: "Kumar", age:35}
+]
+
+const names = users.map(value => value.name)
+const ages = users.map(value => value.age)
+console.log(names,ages)
